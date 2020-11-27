b412ca517b08a3b0918f82bd89c1d346959956d9
Marissa Nuss <mnuss01@rams.shepherd.edu>
2020-11-27 12:40:15 -0500

    Added LED code

--- Final.py
+++ Final.py
@@ -1,4 +1,10 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-print("Welcome!")
+from time import sleep
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.OUT, initial = GPIO.LOW)
+while True:
+    GPIO.output(8, GPIO.HIGH)
+    sleep(1)
+    GPIO.output(8, GPIO.LOW)
+    sleep(1)
