cfa310334fc100c90386538f9f2b92649141d02f
Marissa Nuss <mnuss01@rams.shepherd.edu>
2020-11-28 12:33:00 -0500

    Working Project!

--- Final.py
+++ Final.py
@@ -1,16 +1,20 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-from time import sleep
 def button_callback(channel):
-    print("Button was pushed!")
+    print("Button was pressed!")
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup(8, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(10, GPIO.IN, pull_up_down = GPIO.PUD_DOWN) # Pin number 10
+GPIO.setup(8, GPIO.OUT, initial = GPIO.LOW) # Pin number 8
 GPIO.add_event_detect(10, GPIO.RISING, callback = button_callback) 
-while 10:
-    GPIO.output(8, GPIO.HIGH)
-    sleep(1)
-    GPIO.output(8, GPIO.LOW)
-    sleep(1)
-message = input("Press enter to quit")
+while True:
+    if GPIO.event_detected(10):
+        print(" LED is on.")
+        activate = True
+        while (activate is True):
+            GPIO.output(8, GPIO.HIGH)
+            if GPIO.event_detected(10):
+                activate = False
+        else:
+            GPIO.output(8, GPIO.LOW)
+            print(" LED is off.")
 GPIO.cleanup()
